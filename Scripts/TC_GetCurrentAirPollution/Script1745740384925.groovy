1a8b896bf0191a3f78e684fbaf0b76355e668a87
yahya.hafidz <yahya.hafidz@amarbank.co.id>
2025-04-28 08:16:12 +0700

    test(api): create script to verify current air pollution API using positive and negative cases after keyword implementation

--- Scripts/TC_GetCurrentAirPollution/Script1745740384925.groovy
+++ Scripts/TC_GetCurrentAirPollution/Script1745740384925.groovy
@@ -1,44 +1,9 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import keywords.WeatherKeywords as WK
 import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
-
-import com.kms.katalon.core.testobject.ResponseObject
 
 
-// 1) Send the API request
-ResponseObject response = WS.sendRequest(findTestObject(
-	'Postman/current_air_pollution_in_jakarta',
-	[
-		('apiKey'): GlobalVariable.apiKey,
-		('lat_jaksel')   : GlobalVariable.lat_jaksel,
-		('lon_jaksel')   : GlobalVariable.lon_jaksel
-	]
-))
+// for a happy‐path check:
+WK.getCurrentAirPollution(-6.266, 106.8135, GlobalVariable.apiKey, 200)
 
-// —— Assert #1: HTTP status & non-empty body ——
-WS.verifyResponseStatusCode(response, 200)
-WS.verifyNotEqual(WS.getResponseText(response).trim(), '', 'Response body should not be empty')
-
-
-// print out what Katalon thinks your key is
-println "API key = '${GlobalVariable.apiKey}'"
-
-
-
-// print the exact URL that was called
-println "Called URL: " + response.getUrl()
+// for an “unauthorized” scenario:
+WK.getCurrentAirPollution(-6.266, 106.8135, "bad_key", 401)
