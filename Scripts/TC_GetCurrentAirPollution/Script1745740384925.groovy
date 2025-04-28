fa87b09ebe1e80b03cf092fff893e4aba44e3aa1
yahya.hafidz <yahya.hafidz@amarbank.co.id>
2025-04-28 14:40:58 +0700

    refactor(tc): update air pollution API test script to use dynamic global variables

--- Scripts/TC_GetCurrentAirPollution/Script1745740384925.groovy
+++ Scripts/TC_GetCurrentAirPollution/Script1745740384925.groovy
@@ -1,27 +1,44 @@
+////import keywords.WeatherKeywords as WK
+////import internal.GlobalVariable as GlobalVariable
 //import keywords.WeatherKeywords as WK
-//import internal.GlobalVariable as GlobalVariable
-import keywords.WeatherKeywords as WK
-import internal.GlobalVariable as GV
-//
-//
-// for positive case scenario:
-WK.getCurrentAirPollution("-6.266", "106.8135", GV.apiKey, 200)
-
-// for an "unauthorized" scenario:
-WK.getCurrentAirPollution("-6.266", "106.8135", "bad_key", 401)
-
-
-//// for a "bad request" for latitude scenario:
-WK.getCurrentAirPollution("abc", "106.8135", GV.apiKey, 400)
-
-// for a "bad request" for longitude scenario:
-WK.getCurrentAirPollution("-6.266", "abc", GV.apiKey, 400)
-
-
-
-
-//// 1) happy path → 200 OK
+//import internal.GlobalVariable as GV
+////
+////
+//// for positive case scenario:
 //WK.getCurrentAirPollution("-6.266", "106.8135", GV.apiKey, 200)
 //
-//// 2) bad‐latitude path → 400 Bad Request
-//WK.getCurrentAirPollution("abc",     "106.8135", GV.apiKey, 400)
+////// for an "unauthorized" scenario:
+////WK.getCurrentAirPollution("-6.266", "106.8135", "bad_key", 401)
+//
+//
+////// for a "bad request" for latitude scenario:
+//WK.getCurrentAirPollution("abc", "106.8135", GV.apiKey, 400)
+//
+////// for a "bad request" for longitude scenario:
+////WK.getCurrentAirPollution("-6.266", "abc", GV.apiKey, 400)
+//
+//
+//
+//
+////// 1) happy path → 200 OK
+////WK.getCurrentAirPollution("-6.266", "106.8135", GV.apiKey, 200)
+////
+////// 2) bad‐latitude path → 400 Bad Request
+////WK.getCurrentAirPollution("abc",     "106.8135", GV.apiKey, 400)
+
+
+
+import keywords.WeatherKeywords as WK
+import internal.GlobalVariable as GV
+
+// happy path → 200 OK
+WK.getCurrentAirPollution(GV.lat_jaksel.toString(), GV.lon_jaksel.toString(), GV.apiKey, 200)
+
+// bad-latitude → 400 Bad Request
+WK.getCurrentAirPollution('abc', GV.lon_jaksel.toString(), GV.apiKey, 400)
+
+// bad-longitude → 400 Bad Request
+WK.getCurrentAirPollution(GV.lat_jaksel.toString(), 'xyz', GV.apiKey, 400)
+
+// bad-key → 401 Unauthorized
+WK.getCurrentAirPollution(GV.lat_jaksel.toString(), GV.lon_jaksel.toString(), 'bad_key', 401)
