a8aab3c194e2d0ce8f69e364824fcb81c5bf0f68
yahya.hafidz <yahya.hafidz@amarbank.co.id>
2025-04-28 09:39:55 +0700

    add new scenarios for bad request

--- Scripts/TC_GetCurrentAirPollution/Script1745740384925.groovy
+++ Scripts/TC_GetCurrentAirPollution/Script1745740384925.groovy
@@ -1,9 +1,27 @@
+//import keywords.WeatherKeywords as WK
+//import internal.GlobalVariable as GlobalVariable
 import keywords.WeatherKeywords as WK
-import internal.GlobalVariable as GlobalVariable
+import internal.GlobalVariable as GV
+//
+//
+// for positive case scenario:
+WK.getCurrentAirPollution("-6.266", "106.8135", GV.apiKey, 200)
+
+// for an "unauthorized" scenario:
+WK.getCurrentAirPollution("-6.266", "106.8135", "bad_key", 401)
 
 
-// for a happy‐path check:
-WK.getCurrentAirPollution(-6.266, 106.8135, GlobalVariable.apiKey, 200)
+//// for a "bad request" for latitude scenario:
+WK.getCurrentAirPollution("abc", "106.8135", GV.apiKey, 400)
 
-// for an “unauthorized” scenario:
-WK.getCurrentAirPollution(-6.266, 106.8135, "bad_key", 401)
+// for a "bad request" for longitude scenario:
+WK.getCurrentAirPollution("-6.266", "abc", GV.apiKey, 400)
+
+
+
+
+//// 1) happy path → 200 OK
+//WK.getCurrentAirPollution("-6.266", "106.8135", GV.apiKey, 200)
+//
+//// 2) bad‐latitude path → 400 Bad Request
+//WK.getCurrentAirPollution("abc",     "106.8135", GV.apiKey, 400)
