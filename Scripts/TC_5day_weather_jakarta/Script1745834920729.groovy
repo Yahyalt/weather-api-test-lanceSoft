e2d8fab4ba82b2b4df1f1f437734941642a8a2af
yahya.hafidz <yahya.hafidz@amarbank.co.id>
2025-04-28 18:36:48 +0700

    add test case for weather

--- Scripts/TC_5day_weather_jakarta/Script1745834920729.groovy
+++ Scripts/TC_5day_weather_jakarta/Script1745834920729.groovy
@@ -3,3 +3,12 @@ import internal.GlobalVariable as GV
 
 // happy path → 200 OK
 WK.getFiveDayForecast(GV.lat_jaksel.toString(), GV.lon_jaksel.toString(), GV.apiKey, 200)
+
+// bad-latitude → 400 Bad Request
+WK.getFiveDayForecast('abc', GV.lon_jaksel.toString(), GV.apiKey, 400)
+
+// bad-longitude → 400 Bad Request
+WK.getFiveDayForecast(GV.lat_jaksel.toString(), 'xyz', GV.apiKey, 400)
+
+// bad-key → 401 Unauthorized
+WK.getFiveDayForecast(GV.lat_jaksel.toString(), GV.lon_jaksel.toString(), 'bad_key', 401)
